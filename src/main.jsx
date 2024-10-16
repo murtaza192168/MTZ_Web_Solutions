083af6a58f77bbbdfc7efc9213f5b178718bfc5d
murtaza192168 <murtazacloudwork1252@gmail.com>
2024-10-16 17:03:56 +0530

    Initial commit - frontend setup

---
+++ src/main.jsx
@@ -0,0 +1,12 @@
+// src/main.jsx
+import 'bootstrap/dist/css/bootstrap.min.css';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './index.css'; // Optional, if you want to add custom styles
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+);
